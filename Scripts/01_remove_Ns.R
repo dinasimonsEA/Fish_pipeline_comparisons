d45286f69dbe2d3ce96b82d85e07d5bd47b686f8
dinasimonsEA <dina.simons@environment-agency.gov.uk>
2026-05-12 10:44:22 +0000

    Update gitignore

--- Scripts/01_remove_Ns.R
+++ Scripts/01_remove_Ns.R
@@ -1,1 +1,20 @@
-# Remove N's for DADA2 functionality
+# Remove N's for DADA2 functionality using filter and trim
+
+#get wd & data location
+path<-getwd()
+test_data_loc <- paste("Data/Raw/RingTrial_Sean")
+
+# group raw files into "forward" reads and "reverse" reads
+fnFs <- sort(manifest$absolute_forward_path)
+fnRs <- sort(manifest$absolute_backward_path)
+
+# set up filepaths for output files
+fnFs.filtN <- file.path(path, "Data/Temp/filtN", basename(fnFs)) # Put N-filterd files in filtN/ subdirectory
+fnRs.filtN <- file.path(path, "Data/Temp/filtN", basename(fnRs))
+
+# run filterAndTrim for all files
+eval(parse(text = paste("filterAndTrim(fnFs, fnFs.filtN, fnRs, fnRs.filtN, maxN = 0, ", ")")))
+
+# write objects to pass to next script
+saveRDS(fnFs.filtN, file = paste(path,"/Data/Temp/R_objects/01_fnFs.filtN.rds",sep=""))
+saveRDS(fnRs.filtN, file = paste(path,"/Data/Temp/R_objects/01_fnRs.filtN.rds",sep=""))
