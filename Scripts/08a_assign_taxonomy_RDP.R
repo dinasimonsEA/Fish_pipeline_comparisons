c3dd63ba0b6c9aa69bfbe063ac83e032a83697e4
dinasimonsEA <dina.simons@environment-agency.gov.uk>
2026-05-22 15:35:31 +0000

    renamed script

--- Scripts/08a_assign_taxonomy_RDP.R
+++ Scripts/08a_assign_taxonomy_RDP.R
@@ -8,13 +8,13 @@ seqtab.nochim <- readRDS(file = paste(path, "/Data/Temp/R_objects/06_seqtab.noch
 database <- "Data/Databases/Meta-fish-lib/references.12s.miya.dada.taxonomy.v268.fasta"
 
 # assign taxonomy
-taxa <- assignTaxonomy(seqtab.nochim, database, multithread=TRUE, verbose=T, tryRC = TRUE)
+taxa <- assignTaxonomy(seqtab.nochim, database, tryRC = TRUE, verbose = TRUE, multithread = TRUE)
 
 ## get db_name to add onto output
-db_name <- basename(database)
+db_name <- "Meta-fish-lib"
 
 # write taxonomy table
-write.table(taxa, file = paste(path, "/Results/08_assigned_taxonomy_", db_name, ".csv", sep=""))
+write.table(taxa, file = paste(path, "/Data/Processed/08_assigned_taxonomy_DADA2_", db_name, ".csv", sep=""))
 
 # write out R objects for use later
 saveRDS(taxa, file = paste(path, "/Data/Temp/R_objects/08_taxa.rds", sep=""))
