f5c5bd4dbe05285609d7608240d828c988e9917b
dinasimonsEA <dina.simons@environment-agency.gov.uk>
2026-07-03 15:08:36 +0000

    added metaBEAT and vsearch

--- Scripts/10_create_master_dataset.py
+++ Scripts/10_create_master_dataset.py
@@ -11,7 +11,7 @@ import numpy as np
 datasets = ["Windermere_2017", "Marchamley", "RingTrial_Sean"] # folder names
 base_path = ["Data/Processed"]
 denoise_method = ["DADA2", "VSEARCH", "MetaBEAT"] # denoising methods
-taxonomy_method = ["RDP", "MAPseq", "BLAST"]
+taxonomy_method = ["RDP", "MAPseq", "BLAST", "MetaBEAT", "VSEARCH"] # taxonomy assignment methods 
 base_path = "/Workspace/Users/dina.simons@environment-agency.gov.uk/Fish_pipeline_comparisons/Data/Processed/"
 
 #====================================================================
@@ -94,13 +94,27 @@ for method in denoise_method:
 
 # Combine
 ASV_merged_dataset = pd.concat(master_list, ignore_index=True)
-ASV_merged_dataset = ASV_merged_dataset.drop_duplicates()
 
 # Convert to category (factors)
 for col in ["asv", "sampleid", "denoise_method", "dataset"]:
     ASV_merged_dataset[col] = ASV_merged_dataset[col].astype("category")
 
+#checks
 print("\nRows of master ASV file:", len(ASV_merged_dataset))
+
+# summary
+
+summary = (
+    ASV_merged_dataset
+    .groupby(["denoise_method"])
+    .agg(
+        total_reads=("reads", "sum"),
+        unique_asvs=("asv", "nunique")
+    )
+    .reset_index()
+)
+
+print(summary)
 
 #====================================================================
 # taxonomy master table ---------------------------------------------
@@ -114,18 +128,6 @@ def make_taxonomy_string(df):
     return df[["kingdom","phylum","class","order","family","genus","species"]]\
         .apply(lambda x: ";".join(x.fillna("unclassified").astype(str)), axis=1)
     
-# label mapseq columns
-mapseq_cols = [
-    "asv", "hit_id", "bitscore", "identity", "matches", "mismatches", "gaps",
-    "query_start", "query_end", "dbhit_start", "dbhit_end", "strand", "kingdom", "kingdom_combined_cf", "kingdom_score_cf",
-    "phylum", "phylum_combined_cf", "phylum_score_cf",
-    "class", "class_combined_cf", "class_score_cf",
-    "order", "order_combined_cf", "order_score_cf",
-    "family", "family_combined_cf", "family_score_cf",
-    "genus", "genus_combined_cf", "genus_score_cf",
-    "species", "species_combined_cf", "species_score_cf"
-]
-
 # loop
 
 taxonomy_master_list = []
@@ -145,28 +147,27 @@ for ds in datasets:
 
         # MAPSEQ
         mapseq_file = [
-            f for f in files if "mapseq" in f.lower()
-            and "riaz" in f.lower()
-            and f"_{method_name}_" in f.lower()
-        ]
+            f for f in files 
+            if "mapseq" in f.lower() 
+            and "taxonomy_cf05" in f.lower()
+            and method_name in f.lower()]
 
         if len(mapseq_file) == 1:
-            print(f"\nMAPseq file location: {mapseq_file}")
+            print(f"\nMapseq file location: {mapseq_file}")
 
-            df_map = pd.read_csv(os.path.join(folder_path, mapseq_file[0]), sep="\t", header=0)
-            print("Rows:", len(df_map))
+            df_mapseq = pd.read_csv(os.path.join(folder_path, mapseq_file[0]), sep="\t")
+            print("Rows:", len(df_mapseq))
 
-            df_map = df_map.dropna(axis=1, how="all")
-            df_map.columns = mapseq_cols
-            df_map.columns = map(str.lower, df_map.columns)
+            df_mapseq = df_mapseq.rename(columns={"ASV_ID": "asv"})
+            df_mapseq.columns = map(str.lower, df_mapseq.columns)
 
-            tax_map = df_map[["asv","kingdom","phylum","class","order","family","genus","species"]].copy()
-            tax_map["taxonomy"] = make_taxonomy_string(tax_map)
-            tax_map["taxonomy_method"] = "mapseq"
-            tax_map["dataset"] = dataset_name
-            tax_map["denoise_method"] = method_name
+            tax_mapseq = df_mapseq[["asv","kingdom","phylum","class","order","family","genus","species"]].copy()
+            tax_mapseq["taxonomy"] = make_taxonomy_string(tax_mapseq)
+            tax_mapseq["taxonomy_method"] = "MAPseq"
+            tax_mapseq["dataset"] = dataset_name
+            tax_mapseq["denoise_method"] = method_name
 
-        taxonomy_master_list.append(tax_map)
+            taxonomy_master_list.append(tax_mapseq)
 
         # RDP
         rdp_file = [f for f in files if "assigned_taxonomy" in f.lower() 
@@ -188,11 +189,11 @@ for ds in datasets:
             tax_rdp["dataset"] = dataset_name
             tax_rdp["denoise_method"] = method_name
 
-        taxonomy_rdp_only_list.append(tax_rdp)
+            taxonomy_rdp_only_list.append(tax_rdp)
 
         # LCA
         lca_file = [f for f in files if "lca" in f.lower() 
-                    and "riaz" in f.lower()
+                    and "riaz95_88" in f.lower() # for the 88 min length
                     and method_name in f.lower()]
 
         if len(lca_file) == 1:
@@ -201,7 +202,6 @@ for ds in datasets:
             df_lca = pd.read_csv(os.path.join(folder_path, lca_file[0]), sep="\t")
             print("Rows:", len(df_lca))
 
-            df_lca["kingdom"] = np.nan
             df_lca = df_lca.rename(columns={"ASV_ID": "asv"})
             df_lca.columns = map(str.lower, df_lca.columns)
 
@@ -211,7 +211,94 @@ for ds in datasets:
             tax_lca["dataset"] = dataset_name
             tax_lca["denoise_method"] = method_name
 
-        taxonomy_master_list.append(tax_lca)
+            taxonomy_master_list.append(tax_lca)
+
+        # MetaBEAT
+        metabeat_file = [f for f in files if "otu" in f.lower() 
+                    and "blast_95" in f.lower()
+                    and method_name in f.lower()]
+
+        if len(metabeat_file) == 1:
+            print(f"\nMetaBEAT file location: {metabeat_file}")
+
+            df_lca = pd.read_csv(os.path.join(folder_path, metabeat_file[0]), sep="\t")
+            print("Rows:", len(df_lca))
+
+            df_lca = df_lca.rename(columns={"ASV_ID": "asv"})
+            df_lca.columns = map(str.lower, df_lca.columns)
+
+            tax_lca = df_lca[["asv","kingdom","phylum","class","order","family","genus","species"]].copy()
+            tax_lca["taxonomy"] = make_taxonomy_string(tax_lca)
+            tax_lca["taxonomy_method"] = "MetaBEAT"
+            tax_lca["dataset"] = dataset_name
+            tax_lca["denoise_method"] = method_name
+
+            taxonomy_master_list.append(tax_lca)        
+
+        # VSEARCH
+        vsearch_file = [
+            f for f in files
+            if f.lower().startswith("08_vsearch_output_")
+            and f.lower().endswith("_lca.tsv")
+            and f"output_{method_name.lower()}_" in f.lower()
+        ]
+
+        if len(vsearch_file) == 1:
+            print(f"\nVSEARCH file location: {vsearch_file}")
+
+            df_vsearch = pd.read_csv(
+                os.path.join(folder_path, vsearch_file[0]),
+                sep="\t",
+                header=None,
+                names=["asv", "taxonomy_raw"]
+            )
+
+            print("Rows:", len(df_vsearch))
+
+            # Split taxonomy string into ranks
+            tax_split = (
+                df_vsearch["taxonomy_raw"]
+                .str.rstrip(";")
+                .str.split(";", expand=True)
+            )
+
+            # Keep first 7 taxonomy levels
+            tax_split = tax_split.iloc[:, :7]
+
+            tax_split.columns = [
+                "kingdom",
+                "phylum",
+                "class",
+                "order",
+                "family",
+                "genus",
+                "species"
+            ]
+
+            # Convert placeholders to NA
+            tax_split = tax_split.replace(
+                ["k__", "p__", "c__","o__", "f__", "g__", "s__", "", "NA"],
+                pd.NA
+            )
+
+            df_vsearch = pd.concat(
+                [df_vsearch[["asv"]], tax_split],
+                axis=1
+            )
+
+            df_vsearch.columns = map(str.lower, df_vsearch.columns)
+
+            tax_vsearch = df_vsearch[
+                ["asv", "kingdom", "phylum", "class",
+                "order", "family", "genus", "species"]
+            ].copy()
+
+            tax_vsearch["taxonomy"] = make_taxonomy_string(tax_vsearch)
+            tax_vsearch["taxonomy_method"] = "VSEARCH"
+            tax_vsearch["dataset"] = dataset_name
+            tax_vsearch["denoise_method"] = method_name
+
+            taxonomy_master_list.append(tax_vsearch) 
 
 #====================================================================
 # combine and merge -------------------------------------------------
@@ -233,40 +320,80 @@ taxonomy_rdp_dataset = taxonomy_rdp_dataset.drop_duplicates(
     subset=["sequence","taxonomy_method","dataset","denoise_method"]
 )
 
-# merge
-# rdp first by sequence
-asv_rdp_join = pd.merge(ASV_merged_dataset,
+# all taxonomy methods expected
+methods = taxonomy_rdp_dataset["taxonomy_method"].unique()
+
+# create ASV × taxonomy_method combinations
+asv_expanded = (
+    ASV_merged_dataset.assign(key=1)
+    .merge(
+        pd.DataFrame({"taxonomy_method": methods, "key": 1}),
+        on="key"
+    )
+    .drop("key", axis=1)
+)
+
+# merge taxonomy assignments
+asv_rdp_join = asv_expanded.merge(
     taxonomy_rdp_dataset,
-    on=["sequence","dataset","denoise_method"],
+    on=[
+        "sequence",
+        "dataset",
+        "denoise_method",
+        "taxonomy_method"
+    ],
     how="left"
 )
 
 print("\nFinal RDP rows:", len(asv_rdp_join))
 print("Missing taxonomy:", asv_rdp_join["taxonomy"].isna().sum())
 
-asv_other_join = pd.merge(ASV_merged_dataset,
-    taxonomy_master_dataset,
-    on=["asv","dataset", "denoise_method"],
-    how="left"
+# all taxonomy methods expected
+methods = taxonomy_master_dataset["taxonomy_method"].unique()
+
+# create ASV × taxonomy_method combinations
+asv_expanded = (
+    ASV_merged_dataset.assign(key=1)
+    .merge(
+        pd.DataFrame({"taxonomy_method": methods, "key": 1}),
+        on="key"
+    )
+    .drop("key", axis=1)
 )
 
-asv_other_join = asv_other_join[
-    asv_other_join["taxonomy_method"].notna()
-]
+# merge taxonomy assignments
+asv_other_join = asv_expanded.merge(
+    taxonomy_master_dataset,
+    on=[
+        "asv",
+        "dataset",
+        "denoise_method",
+        "taxonomy_method"
+    ],
+    how="left"
+)
 
 print("\nFinal taxa for mapseq and blast rows:", len(asv_other_join))
 print("Missing taxonomy:", asv_other_join["taxonomy"].isna().sum())
 
 # join the two
 final_master_dataset = pd.concat([asv_rdp_join, asv_other_join], ignore_index=True)
-final_master_dataset = final_master_dataset.drop_duplicates()
-
-#final_master_dataset = final_master_dataset[
-    #final_master_dataset["taxonomy_method"].notna()
-#]
 
 print("\nFinal total rows:", len(final_master_dataset))
 print("Missing taxonomy:", final_master_dataset["taxonomy"].isna().sum())
+
+# summary
+summary = (
+    final_master_dataset
+    .groupby(["denoise_method", "taxonomy_method"])
+    .agg(
+        total_reads=("reads", "sum"),
+        unique_asvs=("asv", "nunique")
+    )
+    .reset_index()
+)
+
+print(summary)
 
 # save
 final_master_dataset.to_csv("Data/Processed/master_long_data.csv", index=False)
